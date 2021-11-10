1d33259d8c25a32cccd94cb6c3867de130076364
Ananye Agarwal <ananayagarwal@gmail.com>
2021-11-10 16:23:48 -0500

    Log additional metrics in the same row

--- ml_runlog/main.py
+++ ml_runlog/main.py
@@ -16,6 +16,8 @@ class SheetLogger:
         self.gc = pygsheets.authorize(service_file=self.creds_path)
         self.sheet = self.gc.open(self.sheet_name)
         self.worksheet = self.sheet[worksheet_idx]
+        self.sno = None
+        self.column_idx = 1
 
     def reorder_cols(self, df):
         cols = df.columns.tolist()
@@ -29,13 +31,19 @@ class SheetLogger:
     def log_data(self, **kwargs):
         """Data is in the form of kwarg=value"""
 
-        sno = self.get_sno() # Starts from zero
-        assert('sno' not in kwargs.keys())
-        kwargs['sno'] = [sno]
-        df = pd.DataFrame(kwargs)
-        df = self.reorder_cols(df)        
+        if self.sno is None:
+            self.sno = self.get_sno() # Starts from zero
+            assert('sno' not in kwargs.keys())
+            kwargs['sno'] = [self.sno]
+            df = pd.DataFrame(kwargs)
+            df = self.reorder_cols(df)  
+        else:
+            first_key = list(kwargs.keys())[0]
+            kwargs[first_key] = [kwargs[first_key]]
+            df = pd.DataFrame(kwargs)
 
-        self.worksheet.set_dataframe(df, (sno + 1, 1), copy_head=False)
+        self.worksheet.set_dataframe(df, (self.sno + 1, self.column_idx), copy_head=False)
+        self.column_idx += len(kwargs)
 
     def get_sno(self):
         with warnings.catch_warnings():
@@ -51,3 +59,5 @@ if __name__ == "__main__":
     logger.log_data(run_id="ornate-course",
                     accuracy="98%",
                     training_time="99")
+
+    logger.log_data(intermediate_accuracy="88")
