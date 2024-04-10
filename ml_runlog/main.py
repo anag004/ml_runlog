c9a2f09d1d099976be5a56db8168b97175da0015
Ananye Agarwal <ananayagarwal@gmail.com>
2024-04-09 22:32:54 -0400

    add v2.0, support logging multiple rows at once

--- ml_runlog/main.py
+++ ml_runlog/main.py
@@ -1,41 +1,81 @@
 import pygsheets
 import pandas as pd 
-import os
-import warnings
-import psutil
 import time
 
-def reorder_cols(df):
-    cols = df.columns.tolist()
-    idx = cols.index('sno')
-    del cols[idx]
-    cols.insert(0, 'sno')
-    df = df[cols]
-
-    return df
-
-def get_sno(worksheet):
-    with warnings.catch_warnings():
-        warnings.simplefilter("ignore")
-        df = worksheet.get_as_df()
+class MLRunlog: 
+    def __init__(self, creds_path_, sheet_name_, worksheet_idx_=0):
+        self.creds_path = creds_path_
+        self.sheet_name = sheet_name_
+        self.worksheet_idx = worksheet_idx_
+        self.gc = pygsheets.authorize(service_file=creds_path_) 
     
-    return len(df) + 1
+    def get_sno(self):
+        sheet = self.gc.open(self.sheet_name)
+        worksheet = sheet[self.worksheet_idx]
+        return len(worksheet.get_as_df()) + 1
 
-def monitor_health(worksheet, sno):
-    while True:
-        parent_pid = os.getppid()
+    def allocate_sno(self, n, sno=None):
+        if sno is None:
+            sno = self.get_sno()
+        self.log_data([{} for _ in range(n)], sno=sno)
+        return list(range(sno, sno + n))
 
-        if parent_pid != 1:
-            status = "ALIVE"
+
+    def log_data(self, data_list=[], verify_timeout=None, sno=None):
+        if len(data_list) == 0:
+            return
+        
+        if sno is None:
+            sno = self.get_sno()
+
+        sheet = self.gc.open(self.sheet_name)
+        worksheet = sheet[self.worksheet_idx]
+
+        for i, row in enumerate(data_list):
+            row['sno'] = sno + i
+
+        # convert list of dicts to dataframe
+        data_df = pd.DataFrame(data_list)
+        data_df = self.move_sno_to_left(data_df)
+
+        worksheet.set_dataframe(data_df.copy(), (sno + 1, 1), copy_head=False)        
+
+        if verify_timeout:
+            start_time = time.time() 
+            while time.time() - start_time < verify_timeout:
+                if self.get_data_at_row(sno).equals(data_df.iloc[[0]]):
+                    print("Data logged successfully")
+                    return
+                print("Data not found, retrying")
+                time.sleep(1)   
+            raise Exception("Data not found after timeout") 
+        
+    def get_data_at_row(self, sno):
+        sheet = self.gc.open(self.sheet_name)
+        worksheet = sheet[self.worksheet_idx]
+        df = worksheet.get_as_df()
+        
+        return df[df['sno'] == sno].reset_index(drop=True)
+
+    def move_sno_to_left(self, df):
+        cols = df.columns.tolist()
+        idx = cols.index('sno')
+        del cols[idx]
+        cols.insert(0, 'sno')
+        df = df[cols] 
+        df['sno'] = df['sno'].astype('int64') # this is how pygsheets reads it 
+
+        return df
+    
+    def clear_sheet(self, retain_header=True):
+        sheet = self.gc.open(self.sheet_name)
+        worksheet = sheet[self.worksheet_idx]
+        if retain_header:   
+            worksheet.clear(start='A2', end='ZZ100000')
         else:
-            status = "DEAD"
+            worksheet.clear()
 
-        status_dict = {"status": [status]}
-        df = pd.DataFrame(status_dict)
-        worksheet.set_dataframe(df, (sno + 1, 1), copy_head=False)
 
-        if status == "DEAD":
-            print("Parent died, terminating ml_runlog")
-            exit()
+# write integration tests
 
-        time.sleep(0.1)
+
--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup
 
 setup(name='ml_runlog',
-      version='1.9',
-      description='Simple script to log experiments to Google Sheets',
+      version='2.0',
+      description='Simple script to log experiments to Google Sheets. Version 2.0 is cleaner, has a class based structure and supports logging multiple rows at once.',
       url='https://github.com/anag004/ml-runlog',
       author='Ananye Agarwal',
       author_email='ananayagarwal@gmail.com',
